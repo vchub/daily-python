7113f881912e5b9871118d246880e6716d7f6d22
vlad chub <vlad.chub@gmail.com>
2018-12-23 21:31:20 -0800

    inorder_iter

--- generators/chub/test_traverse_tree.py
+++ generators/chub/test_traverse_tree.py
@@ -13,6 +13,19 @@ class Node(object):
     r: Any = None
 
 
+def add1(x: Node, v: E) -> None:
+    t = x
+    while x and v < x.v:
+        x, t = x.l, x
+    while x and v > x.v:
+        x, t = x.r, x
+    if not x:
+        if v < t.v:
+            t.l = Node(v)
+        else:
+            t.r = Node(v)
+
+
 def add(x: Node, v: E) -> Node:
     if not x:
         return Node(v)
@@ -23,13 +36,47 @@ def add(x: Node, v: E) -> Node:
     return x
 
 
-def inorder(node: Node) -> Iterator[E]:
-    if node:
-        for x in inorder(node.l):
-            yield x
+def inorder_iter(node: Node) -> Iterator:
+    # https://github.com/python/cpython/blob/3.6/Lib/test/test_generators.py
+    st = []
+    while node:
+        while node.l:
+            st.append(node)
+            node = node.l
         yield node.v
-        for x in inorder(node.r):
-            yield x
+        while not node.r:
+            try:
+                node = st.pop()
+            except IndexError as e:
+                return
+            yield node.v
+
+        node = node.r
+
+
+def inorder_recur(node: Node) -> Generator[E, E, None]:
+    if not node:
+        return
+    for x in inorder_recur(node.l):
+        yield x
+    yield node.v
+    for x in inorder_recur(node.r):
+        yield x
+
+
+def test_inorder_iter():
+    N = 3
+    root = Node(0)
+    for i in range(N):
+        add1(root, i)
+    assert list(inorder_iter(root)) == list(range(N))
+
+    # N = 1000
+    # root = Node(0)
+    # for i in range(N):
+    #     add1(root, i)
+    #
+    # assert list(inorder_iter(root)) == list(range(N))
 
 
 def test():
@@ -37,7 +84,15 @@ def test():
     N = 3
     for i in range(N):
         root = add(root, i)
-    assert list(inorder(root)) == list(range(N))
+    assert list(inorder_recur(root)) == list(range(N))
+
+
+def test_add1():
+    N = 3
+    root = Node(0)
+    for i in range(N):
+        add1(root, i)
+    assert list(inorder_recur(root)) == list(range(N))
 
 
 def xtest_busting_recursion():
