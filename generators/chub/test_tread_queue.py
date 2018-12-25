c5b9b7d6d32162823a276f8bf5a15d7a85e206da
vlad chub <vlad.chub@gmail.com>
2018-12-24 20:21:18 -0800

    multiple sources

--- generators/chub/test_tread_queue.py
+++ generators/chub/test_tread_queue.py
@@ -1,3 +1,4 @@
+import itertools as it
 import threading
 import time
 from collections import deque
@@ -9,6 +10,11 @@ from typing import Callable, Deque, Iterator, List
 class Frame:
     n: int
     s: str
+
+
+def gen_cat(sources: List[Iterator]) -> Iterator[Frame]:
+    for s in sources:
+        yield from s
 
 
 def sendto_queue(source: Iterator, q: Deque[Frame]) -> None:
@@ -32,7 +38,9 @@ def filter_queue(q: Deque[Frame], pred: Callable[[Frame], bool],
 
 
 def run_th(acc: Iterator) -> None:
-    frames = (Frame(i, f's: {i}') for i in range(5))
+    frame_sources = [(Frame(i, f's: {i}') for i in range(5)),
+                     (Frame(i, f's: {i}') for i in range(5, 10))]
+    frames = gen_cat(frame_sources)
     q: Deque = deque()
 
     def odd(x: Frame) -> bool:
@@ -50,5 +58,5 @@ def test0():
     acc = [None]
     run_th(acc)
     got = list(acc[0])
-    assert len(got) == 3
+    assert len(got) == 5
     print(got)
